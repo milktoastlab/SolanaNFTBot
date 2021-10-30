dee0865c3d87f340aff360f37fd8569ca11c0419
KryptoJ <kryptoj@pobox.com>
2021-10-30 23:34:31 +1100

    Add a catch all marketplace

--- src/lib/marketplaces/index.ts
+++ src/lib/marketplaces/index.ts
@@ -1,7 +1,10 @@
 import { Marketplace } from "./types";
 import magicEden from "./magicEden";
+import digitalEyes from "./digitalEyes";
+import solanart from "./solanart";
+import unknown from "./unknown";
 export * from "./types";
 export { default as parseNFTSale } from "./parseNFTSaleForAllMarkets";
-const marketplaces: Marketplace[] = [magicEden];
+const marketplaces: Marketplace[] = [magicEden, digitalEyes, solanart, unknown];
 
 export default marketplaces;
