c82ee9e326df290fb11b52f5bcc138f4e3164ae2
KryptoJ <kryptoj@pobox.com>
2021-11-07 21:30:53 +1100

    Use const to improve code readability

--- src/lib/marketplaces/helper.ts
+++ src/lib/marketplaces/helper.ts
@@ -38,12 +38,11 @@ export function parseNFTSaleOnTx(
   if (!txResp.meta?.logMessages) {
     return null;
   }
+  const transactionExecByMarketplaceProgram = txResp.meta.logMessages.filter(
+    (msg) => msg.includes(marketplace.programId)
+  ).length;
 
-  if (
-    !txResp.meta.logMessages.filter((msg) =>
-      msg.includes(marketplace.programId)
-    ).length
-  ) {
+  if (!transactionExecByMarketplaceProgram) {
     return null;
   }
   const { innerInstructions } = txResp.meta;
