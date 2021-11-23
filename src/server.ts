9282898ab66a5fabfd168c57cc7fe0bdea5554c1
KryptoJ <kryptoj@pobox.com>
2021-11-23 17:16:03 +1100

    Don't hardcode PORT in yarn command

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import { loadConfig } from "config";
 import { Worker } from "workers/types";
 import notifyNFTSalesWorker from "workers/notifyNFTSalesWorker";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 (async () => {
   try {
