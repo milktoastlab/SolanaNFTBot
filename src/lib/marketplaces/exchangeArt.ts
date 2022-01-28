9f020c7aa9b32f731a8ebd0330860d12f1671b87
SolBots <97853296+SolBotsCo@users.noreply.github.com>
2022-01-28 10:19:30 +0000

    Update exchangeArt.ts

--- src/lib/marketplaces/exchangeArt.ts
+++ src/lib/marketplaces/exchangeArt.ts
@@ -3,6 +3,7 @@ import { parseNFTSaleOnTx } from "./helper";
 
 const exchangeArt: Marketplace = {
   name: "Exchange Art",
+  iconUrl: "https://cdn.exchange.art/static/apple-meta-logo.png"
   programId: ["AmK5g2XcyptVLCFESBCJqoSfwV3znGoVYQnqEnaAZKWn"],
   itemURL: (token: String) => `https://exchange.art/single/${token}`,
   parseNFTSale(web3Conn, txResp): Promise<NFTSale | null> {
