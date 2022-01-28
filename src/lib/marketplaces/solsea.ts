b4ca99b8b314d7225c3aa903c1da56057f2f3ca8
SolBots <97853296+SolBotsCo@users.noreply.github.com>
2022-01-28 10:21:18 +0000

    Update solsea.ts

--- src/lib/marketplaces/solsea.ts
+++ src/lib/marketplaces/solsea.ts
@@ -3,6 +3,7 @@ import { parseNFTSaleOnTx } from "./helper";
 
 const solsea: Marketplace = {
   name: "Solsea",
+  iconUrl: "https://solsea.io/logo_solo.svg",
   programId: ["617jbWo616ggkDxvW1Le8pV38XLbVSyWY8ae6QUmGBAU"],
   itemURL: (token: String) => `https://solsea.io/nft/${token}`,
   parseNFTSale(web3Conn, txResp): Promise<NFTSale | null> {
